16eef103edd624af4aea6462679b28e79e56ce99
liruinan@cmcm.com <liruinan@cmcm.com>
2018-03-30 11:29:01 +0800

    feat: koa集成pug模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+// const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
 // const ejs = require('ejs')
-const pug = require('pug')
+// const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+// views()中两个参数，第一个参数是模板的路径，第二个参数，只要是后缀名是pug的文件，就会被识别成模板文件
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8'
+    // ctx.type = 'text/html; charset=utf-8'
     // ctx.body = ejs.render(ejsTpl, {
     //     you: 'Luke',
     //     me: 'LRN'
     // })
-    ctx.body = pug.render(pugTpl, {
-        you: 'LUKEEEEEE',
-        me: 'liiiiiku'
+    // ctx.body = pug.render(pugTpl, {
+    //     you: 'LUKEEEEEE',
+    //     me: 'liiiiiku'
+    // })
+    // 这个时候render已经被views挂载到了应用上线文context上
+    await ctx.render('index', {
+        you: 'world',
+        me: 'koa2'
     })
 })
 
