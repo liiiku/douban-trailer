107d09ad5c2888c9189515a6f3a3a8ae909be039
liruinan@cmcm.com <liruinan@cmcm.com>
2018-03-30 10:35:19 +0800

    feat: 新增html模板引擎

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const Koa = require('koa')
 const app = new Koa()
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+const pug = require('pug')
 
 app.use(async (ctx, next) => {
-    ctx.body = '电影首页'
+    ctx.type = 'text/html; charset=utf-8'
+    // ctx.body = ejs.render(ejsTpl, {
+    //     you: 'Luke',
+    //     me: 'LRN'
+    // })
+    ctx.body = pug.render(pugTpl, {
+        you: 'LUKEEEEEE',
+        me: 'liiiiiku'
+    })
 })
 
 app.listen(4455)
---
+++ server/tpl/ejs.js
@@ -0,0 +1,26 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html>
+        <header>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Koa Server HTML</title>
+            <link href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+            <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+        </header>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Hi <%= you %></h1>
+                        <p>This is <%= me %></p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>测试动态 EJS 模板引擎</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
+`
