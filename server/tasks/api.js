1603347ce66ee622aa7e78dd7c06615b1dd9ec88
liruinan@cmcm.com <liruinan@cmcm.com>
2018-04-02 23:58:12 +0800

    feat: 新增预告片详情页视频和封面爬取功能

--- server/tasks/api.js
+++ server/tasks/api.js
@@ -3,7 +3,7 @@
 const rp = require('request-promise-native')
 
 async function fetchMovie (item) {
-    const url = `http://api.douban.com//v2/movie/subject/${item.doubanId}`
+    const url = `http://api.douban.com/v2/movie/subject/${item.doubanId}`
     const res = await rp(url)
 
     return res
---
+++ server/tasks/trailer.js
@@ -0,0 +1,29 @@
+const cp = require('child_process')
+const { resolve } = require('path')
+
+;(async () => {
+    const script = resolve(__dirname, '../crawler/video')
+    const child = cp.fork(script, [])  // cp.fork 可以派生出一个子进程
+    let invoked = false  // 表示爬虫脚本有没有被运行过
+
+    child.on('error', err => {
+        if (invoked) return
+        
+        invoked = true
+
+        console.log(err)
+    })
+
+    child.on('exit', code => {
+        if (invoked) return
+
+        invoked = true
+        let err = code === 0 ? null : new Error('exit code ' +  code)
+
+        console.log(err)
+    })
+
+    child.on('message', data => {
+        console.log(29, data)
+    })
+})()
